581227f75cf35d81b5cf9735b60428d3445a8936
edzenfrance <68797488+edzenfrance@users.noreply.github.com>
2023-12-07 23:30:58 +0800

    Update capture Add crosshair icon during screenshot capture Implement auto-resize for label dimensions Add 1 pixel in width and height Play sound only when OCR'ed text is present

--- src/ui/capture.py
+++ src/ui/capture.py
@@ -1,4 +1,5 @@
 # Standard libraries
+import sys
 import tempfile
 from datetime import datetime
 from pathlib import Path
@@ -30,9 +31,10 @@ class ImageLabel(QLabel):
         self.start_capture_mode = False
 
         self.label_dimensions = QLabel(self)
-        self.label_dimensions.setStyleSheet("QLabel { color : white; background-color: rgba(0, 100, 0, 255); }")
+        self.label_dimensions.setStyleSheet("QLabel { color : black; background-color: rgba(248, 248, 186, 255); "
+                                            "border-bottom: 2px solid gray; border-right: 2px solid gray; }")
         self.label_dimensions.setAlignment(Qt.AlignCenter)
-        self.label_dimensions.setGeometry(0, 0, 140, 20)
+        self.label_dimensions.setGeometry(0, 0, 65, 20)
         self.label_dimensions.hide()
 
         self.timer = QTimer()
@@ -55,7 +57,7 @@ class ImageLabel(QLabel):
         super().paintEvent(event)
         if self.selection_area:
             painter = QPainter(self)
-            border_color = QColor('green')
+            border_color = QColor(117, 255, 255, 255)
             border_width = 1
             painter.setPen(QPen(border_color, border_width, Qt.SolidLine))
             painter.drawRect(self.selection_area)
@@ -67,18 +69,17 @@ class ImageLabel(QLabel):
             self.selection_area = QRect(self.start_pos, self.end_pos)
             self.previous_pos = self.end_pos
 
-            width = abs(self.start_pos.x() - self.end_pos.x())
-            height = abs(self.start_pos.y() - self.end_pos.y())
-            self.label_dimensions.setText(f"Width: {width}, Height: {height}")
+            width = abs(self.start_pos.x() - self.end_pos.x()) + 1
+            height = abs(self.start_pos.y() - self.end_pos.y()) + 1
+            self.label_dimensions.setText(f"{width} x {height}")
             self.update_label_position()
-
             self.update()
 
     def update_label_position(self):
-        label_width = self.label_dimensions.width()
+        label_width = self.label_dimensions.sizeHint().width() + 8  # Auto-adjust label width based on text length
         label_height = self.label_dimensions.height()
         label_x = self.selection_area.center().x() - label_width / 2
-        label_y = self.selection_area.bottom() + 5  # Distance from the bottom
+        label_y = self.selection_area.bottom() + 4  # Distance from the bottom
         self.label_dimensions.setGeometry(label_x, label_y, label_width, label_height)
         self.label_dimensions.show()
 
@@ -91,6 +92,7 @@ class GetScreenshot(QMainWindow):
         self.image_label = None
         self.image_layout = None
         self.central_widget = None
+        self.crosshair_cursor = None
         self.extracted_text = None
         self.selection_area = None
         self.start_pos = None
@@ -99,6 +101,10 @@ class GetScreenshot(QMainWindow):
         self.ocr_text_ui = OCRTextUI()
         self.main_ui_instance = main_ui_instance
 
+        self.init_image_label()
+        self.init_crosshair_cursor()
+
+    def init_image_label(self):
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
 
@@ -108,8 +114,32 @@ class GetScreenshot(QMainWindow):
         self.image_label = ImageLabel(self.central_widget)
         self.image_layout.addWidget(self.image_label, alignment=Qt.AlignCenter)
 
-        self.image_label.setContentsMargins(0, 0, 0, 0)
-        self.image_label.setStyleSheet("QLabel { border: none; }")
+    def init_crosshair_cursor(self):
+        crosshair_size = 25
+
+        pixmap_size = crosshair_size * 4
+        pixmap = QPixmap(pixmap_size, pixmap_size)
+        pixmap.fill(Qt.transparent)
+
+        painter = QPainter(pixmap)
+        pen = painter.pen()
+        pen.setWidth(1)  # Thickness of the crosshair
+        pen.setColor(QColor(177, 255, 255, 255))
+        painter.setPen(pen)
+
+        # Center point of the pixmap
+        center_x = pixmap_size // 2
+        center_y = pixmap_size // 2
+
+        # Half-size of the crosshair lines
+        half_size = (crosshair_size - 1) // 2
+
+        # Draw the crosshair on the pixmap
+        painter.drawLine(center_x - half_size, center_y, center_x + half_size, center_y)  # Horizontal line
+        painter.drawLine(center_x, center_y - half_size, center_x, center_y + half_size)  # Vertical line
+        painter.end()
+
+        self.crosshair_cursor = QCursor(pixmap, center_x - 2, center_y - 2)
 
     def close_fullscreen_show_main(self):
         self.close()
@@ -129,6 +159,7 @@ class GetScreenshot(QMainWindow):
 
         self.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
         self.showFullScreen()
+        self.setCursor(self.crosshair_cursor)
 
     def mouseReleaseEvent(self, event):
         if event.button() == Qt.LeftButton:
@@ -146,9 +177,8 @@ class GetScreenshot(QMainWindow):
             self.close_fullscreen_show_main()
 
     def take_screenshot(self):
-        if (not self.image_label.end_pos
-                or self.image_label.start_pos.x() == self.image_label.end_pos.x()
-                or self.image_label.start_pos.y() == self.image_label.end_pos.y()):
+        if (self.image_label.start_pos.x() == self.image_label.end_pos.x() and self.image_label.start_pos.y() == self.image_label.end_pos.y()
+                or not self.image_label.end_pos):
             self.image_label.start_pos = None
             self.image_label.end_pos = None
             return
@@ -167,21 +197,21 @@ class GetScreenshot(QMainWindow):
     def calculate_selected_area(self):
         x = min(self.image_label.start_pos.x(), self.image_label.end_pos.x())
         y = min(self.image_label.start_pos.y(), self.image_label.end_pos.y())
-        width = abs(self.image_label.start_pos.x() - self.image_label.end_pos.x())
-        height = abs(self.image_label.start_pos.y() - self.image_label.end_pos.y())
+        width = abs(self.image_label.start_pos.x() - self.image_label.end_pos.x()) + 1
+        height = abs(self.image_label.start_pos.y() - self.image_label.end_pos.y()) + 1
+        logger.info(f"Selected area: x: {x}, y: {y}, width: {width}, height: {height}")
         return x, y, width, height
 
     def capture_selected_area(self, x, y, width, height):
-        logger.info(f"Selected area: x: {x}, y: {y}, width: {width}, height: {height}")
-
         self.config = load_config()
-        current_datetime = get_current_datetime()
+        current_datetime = self.get_current_datetime()
+        output_folder = self.get_output_folder_path()
 
         screenshot = ImageGrab.grab(bbox=(x, y, x + width, y + height))  # Get a screenshot of the selected area
-        output_folder = self.get_output_folder_path()
 
         if self.config['output']['save_captured_image']:
             captured_file_name = current_datetime + ".png"
+
             try:
                 screenshot.save(output_folder / captured_file_name)
                 logger.info(f"Screenshot saved: {output_folder}\\{captured_file_name}")
@@ -196,17 +226,11 @@ class GetScreenshot(QMainWindow):
             screenshot.save(enhanced_file_name)
             logger.info(f"Screenshot saved as temporary file: {enhanced_file_name}")
 
-        if self.config['preferences']['enable_sound']:
-            play_sound_file(self.config['preferences']['sound_file'])
-
         self.extracted_text = perform_ocr(enhanced_file_name, current_datetime)
+
+        self.play_sound_file()
         self.close_fullscreen_show_main()
-
-        if self.extracted_text[0]:
-            logger.success("OCR completed")
-
-            if self.config['output']['show_popup_window']:
-                self.show_ocr_text_ui()
+        self.show_ocr_text_ui()
 
     def get_output_folder_path(self):
         output_folder = Path(self.config['output']['output_folder_path'])
@@ -224,36 +248,34 @@ class GetScreenshot(QMainWindow):
         return output_folder
 
     def show_ocr_text_ui(self):
-        self.ocr_text_ui.init_ui()
-        if not self.ocr_text_ui.isVisible():
-            self.ocr_text_ui.show()
-        else:
-            self.ocr_text_ui.raise_()
-        self.ocr_text_ui.set_extracted_text(self.extracted_text[0])
-        if self.extracted_text[1] is not None:
-            self.ocr_text_ui.set_translated_text(self.extracted_text[1])
+        if self.extracted_text[0]:
+            logger.success("OCR completed")
 
+            if self.config['output']['show_popup_window']:
+                self.ocr_text_ui.init_ui()
+                self.ocr_text_ui.show() if not self.ocr_text_ui.isVisible() else self.ocr_text_ui.raise_()
+                self.ocr_text_ui.set_extracted_text(self.extracted_text[0])
+                self.ocr_text_ui.set_translated_text(self.extracted_text[1])
 
-def play_sound_file(sound_file):
-    if Path(sound_file).exists():
-        try:
-            playsound(sound_file.replace('\\', '/'), False)
-            logger.success(f"Sound file has been played successfully using playsound")
+    def play_sound_file(self):
+        if not self.extracted_text[0]:
+            return
 
-        except PlaysoundException as e:
-            logger.error(f"An error occurred while playing the sound file '{sound_file}' {e} ")
-    else:
-        logger.error(f"{sound_file} doesnt exist")
+        if self.config['preferences']['enable_sound']:
+            sound_file = self.config['preferences']['sound_file']
 
+            if Path(sound_file).exists():
+                try:
+                    playsound(sound_file.replace('\\', '/'), False)
+                    logger.success(f"Sound file has been played successfully using playsound")
 
-def get_current_datetime():
-    now = datetime.now()
-    year = now.year
-    month = now.month
-    day = now.day
-    hour = now.hour  # 24-hour format (military hour)
-    minute = now.minute
-    second = now.second
-    current_datetime = f"{year}{month:02d}{day:02d}_{hour:02d}{minute:02d}{second:02d}"
-    logger.info(f"Current date and time: {current_datetime}")
-    return current_datetime
+                except PlaysoundException as e:
+                    logger.error(f"An error occurred while playing the sound file '{sound_file}' {e} ")
+            else:
+                logger.error(f"{sound_file} doesnt exist")
+
+    @staticmethod
+    def get_current_datetime():
+        now = datetime.now()
+        # The hour is in a 24-hour format (military time)
+        return f"{now.year}{now.month:02d}{now.day:02d}_{now.hour:02d}{now.minute:02d}{now.second:02d}"
